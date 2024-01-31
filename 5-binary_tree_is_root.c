594fbeaf5e245cfdef6179cc1eb3dacf2adc7e6f
Ehabw57 <maestro.ehab123@gmail.com>
2024-01-31 19:58:19 +0200

    Uploaded check root function

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Checks weather a spacifc node is root or not
+ * @node: the node to be checked
+ * Return: 1 if the node is root 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent)
+		return (0);
+	return (1);
+}
