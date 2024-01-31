34393e182c3dc49ec660bb5885689b47b04bbdb7
Ehabw57 <maestro.ehab123@gmail.com>
2024-01-31 18:11:26 +0200

    Uploded delete tree function

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_t - Clears a whole binary tree
+ * @tree: pointer to the root node of the tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *clear = tree;
+
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(clear->left);
+	binary_tree_delete(clear->right);
+	free(clear);
+}
