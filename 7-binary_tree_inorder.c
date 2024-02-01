f25bfd05bee2b089d2d20c0c82878ea16e6660eb
Ehabw57 <maestro.ehab123@gmail.com>
2024-02-01 17:17:32 +0200

    Uploded travrise inorder funciton

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Traveris and print tree with preorder style
+ * binary_tree_inorder - Traveris and print tree with inord style
  * @tree: Pointer to the root of the tree
  * @func: Printing funciton (idk why we can't just user dirctly printf)
  */
-void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {
 	const binary_tree_t *current = tree;
 
 	if (current == NULL || func == NULL)
 		return;
 
+	binary_tree_inorder(current->left, func);
 	(*func)(current->n);
-	binary_tree_preorder(current->left, func);
-	binary_tree_preorder(current->right, func);
+	binary_tree_inorder(current->right, func);
 }
